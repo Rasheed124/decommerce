0074c22086c208d30930dd6a198b57d3bf9b21af
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-03-01 12:44:03 +0100

    sanity deployed to admin

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -4,6 +4,8 @@
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
+// const token =  process
+
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
     // Live content is currently only available on the experimental API
