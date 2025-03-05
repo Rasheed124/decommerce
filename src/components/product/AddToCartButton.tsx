9a61b838a84859c3200e7364046363d673e3f889
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-03-05 14:15:45 +0100

    category page completed

---
+++ src/components/product/AddToCartButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+// import { formatPrice } from '@/lib/utils'
+// import { Product } from '@/sanity.types'
+// import { urlFor } from '@/sanity/lib/image';
+// import { useCartStore } from '@/stores/cart-store';
+// import { Loader2 } from 'lucide-react'
+// import React, { useState } from 'react'
+// import { useShallow } from 'zustand/shallow'
+
+// type AddToCartButtonProps = {
+//     product: Product
+// }
+const AddToCartButton = () => {
+ 
+
+  return (
+    <button>
+        Add TO cart
+    </button>
+  )
+}
+
+export default AddToCartButton
