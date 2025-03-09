c111fd63a7c45dcdbd64f3b75318b253d217b549
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-03-09 22:15:10 +0100

    adding cart items

--- src/actions/cart-actions.ts
+++ src/actions/cart-actions.ts
@@ -221,16 +221,3 @@ export const syncCartWithUser = async (cartId: string | null) => {
     return getOrCreateCart(existingUserCart.id);
 }
 
-export const addWinningItemToCart = async (cartId: string, product: Product) => {
-
-    const cart = await getOrCreateCart(cartId);
-
-    const updatedCart = await updateCartItem(cart.id, product._id, {
-        title: `🎁 ${product.title} (Won)`,
-        price: 0,
-        image: product.image ? urlFor(product.image).url() : '',
-        quantity: 1,
-    });
-
-    return updatedCart;
-}
